41b0d374ab8107da594f1b9d7f9e41c183849f68
JayPatel-2023 <mr.jaypatel2002@gmail.com>
2023-12-20 04:58:23 +0530

    data passing from quiz to result screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -42,7 +42,7 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'result-screen') {
-      screenWidget = const ResultScreen();
+      screenWidget = ResultScreen(chosenAnswers: selectedAnswers);
     }
 
     return MaterialApp(
